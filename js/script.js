348c1e201aba900d183b279baeb222b7941811cc
MarieSchriver <eaa24masb@students.eaaa.dk>
2025-01-27 09:45:36 +0100

    c

--- js/script.js
+++ js/script.js
@@ -6,4 +6,4 @@ const greetingMessage = "I'm learning to code with JavaScript";
 const greetingMessage = "I want to become a good JavaScript programmer";
   
 
-    console.log(error.message);}
+    console.log(error.message);
