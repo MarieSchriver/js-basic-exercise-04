7572e20332ada2a487729e4682bcae5fabd0012a
MarieSchriver <eaa24masb@students.eaaa.dk>
2025-01-27 09:44:55 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const greetingMessage = "I'm learning to code with JavaScript";
+
+
+const greetingMessage = "I want to become a good JavaScript programmer";
+  
+
+    console.log(error.message);}
